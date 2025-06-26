63419a937c8f56940e1dfa1629d0f2532d00082d
Karla G. <k.grajalesluna@gmail.com>
2025-06-26 15:12:19 +0100

    feat: UI dropdown for user and section for info completed

--- script.mjs
+++ script.mjs
@@ -4,8 +4,64 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { countUsers } from "./common.mjs";
+import { getUserIDs } from "./data.mjs";
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
-};
+let userSelect;
+let userDataDisplay;
+
+/**
+ * create the entire application UI Dynamically using Dom 
+ */
+
+function createUI() {
+  const main = document.createElement("main");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "Music Listening Track";
+  main.appendChild(h1);
+
+  // ------------------- dropdown ---------------------------
+  const dropdownSection = document.createElement("section");
+  const label = document.createElement("label");
+  label.setAttribute("for", "user-select");
+  label.textContent = "User : ";
+  dropdownSection.appendChild(label);
+
+  // ------------------ User Selectors -----------------------
+  userSelect = document.createElement("select");
+  userSelect.id = "user-select";
+
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "  -- Please Choose A User 🎧 -- ";
+  userSelect.appendChild(defaultOption);
+
+  // ----------- Append Select to its Section  ----------------
+  dropdownSection.appendChild(userSelect);
+  main.appendChild(dropdownSection);
+
+  // --------- Create Section to display userData --------------
+  userDataDisplay = document.createElement("section");
+  userDataDisplay.id = "user-date-display";
+
+  const initialMessage = document.createElement("p");
+  initialMessage.textContent =
+    "Please select a user from the dropdown above to view their music listening data.";
+  userDataDisplay.appendChild(initialMessage);
+
+  // ---------------  append data to section main  ----------------
+  main.appendChild(userDataDisplay);
+
+  // ---------------  append entire main to body   ----------------
+  document.body.appendChild(main);
+}
+
+
+
+
+
+
+
+
+console.log("script.mjs loaded");
+createUI();
